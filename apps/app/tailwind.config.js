15abccfa0cd38c85f7d1569a949e94b19ab30a28
Alek Angelov <alek.angelov.sk@gmail.com>
2024-05-13 00:13:19 +0100

    feat: tailwind

---
+++ apps/app/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        tertiary: "var(--tertiary-color)",
+        success: "var(--success-color)",
+        warning: "var(--warning-color)",
+        error: "var(--error-color)",
+        info: "var(--info-color)",
+        background: "var(--background-color)",
+        surface: "var(--surface-color)",
+        text: "var(--text-color)",
+        "text-secondary": "var(--text-color)",
+        "text-tertiary": "var(--text-color)",
+        on: "var(--on-color)",
+      },
+    },
+  },
+  plugins: [],
+};
