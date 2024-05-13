ff87f62508063f2db2e1b7033feaa6958ceddbe1
Alek Angelov <alek.angelov.sk@gmail.com>
2024-05-13 11:10:00 +0200

    x

--- apps/app/src/app.tsx
+++ apps/app/src/app.tsx
@@ -1,10 +1,14 @@
 import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import { Suspense,  onMount } from "solid-js";
 import "./app.css";
 
 export default function App() {
+  onMount(() => {
+    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', systemTheme);
+  })
   return (
     <Router
       root={(props) => (
