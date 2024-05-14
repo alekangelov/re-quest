07a90cff3f92365f43291f798f18215b445afd3b
Alek Angelov <alek.angelov.sk@gmail.com>
2024-05-14 10:54:32 +0200

    changing src-tauri dir and some other stuff

---
+++ apps/app/src/components/Layout/index.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "./Sidebar";
+
+export default function Layout() {
+  return (
+    <main class="w-full h-full">
+      <Sidebar />
+    </main>
+  );
+}
--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -1,19 +1,9 @@
-import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import Layout from "~/components/Layout";
 
 export default function Home() {
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+    <Layout>
+      
+    </Layout>
   );
 }
